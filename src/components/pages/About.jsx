b881e7306d12dfe72fc1c900518ffebcc22f874d
Ademola Afolabi <afolabiademola27@gmail.com>
2024-04-03 13:02:21 +0100

    added link to about button

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,4 +1,5 @@
 import aboutImage from "../pages/van life bg 2.png"
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -12,7 +13,9 @@ const About = () => {
 
         <div style={{background: "rgba(255, 204, 141, 1)", padding: "100px", margin: "50px 50px 100px 50px", borderRadius: "10px"}}>
           <p style={{fontSize: "54px", fontWeight: "700", lineHeight: "40px"}}>Your destination is waiting. <br /><br /> Your van is ready.</p>
-          <button style={{background: "#000", color: "#fff", padding: "30px 40px", outline: "none", border: "none", borderRadius: "10px", margin: "100px 0px 0px 0px", fontSize: "30px", fontWeight: "600", cursor: "pointer"}}>Explore our vans</button>
+          <Link to="/Vans">
+            <button style={{background: "#000", color: "#fff", padding: "30px 40px", outline: "none", border: "none", borderRadius: "10px", margin: "100px 0px 0px 0px", fontSize: "30px", fontWeight: "600", cursor: "pointer"}}>Explore our vans</button>
+          </Link>
         </div>
     </>
   )
